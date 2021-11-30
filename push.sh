e2a06462f7130fb64bdb60937a4b6052f3f89c8a
Vallghall <romangusev2001@mail.ru>
2021-11-30 13:58:05 +0300

    Add solutions for 1-6 and fulfill solution for 25th

---
+++ push.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+git add .
+git commit -m "$1"
+git pull --rebase
+git push --force
--- training/25.sql
+++ training/25.sql
@@ -4,4 +4,6 @@ JOIN PC pc
     ON p.model=pc.model
 WHERE 
   pc.ram = (SELECT MIN(ram) FROM PC) AND 
-  pc.speed = (SELECT MAX(speed) FROM PC WHERE ram = (SELECT MIN(ram) FROM PC));
+  pc.speed = (SELECT MAX(speed) FROM PC WHERE ram = (SELECT MIN(ram) FROM PC))
+INTERSECT
+SELECT DISTINCT maker FROM Product WHERE type='Printer'
