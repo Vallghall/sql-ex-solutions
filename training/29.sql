f9e251af9b0f8c415801c7fd2db5d3d96741e02f
Vallghall <romangusev2001@mail.ru>
2021-12-03 13:25:31 +0300

    Add solutions for 29

---
+++ training/29.sql
@@ -0,0 +1,11 @@
+SELECT i.point, i.date, SUM(i.inc), SUM(o.out)
+    FROM Income_o i
+    LEFT JOIN Outcome_o o
+        ON i.point=o.point AND i.date=o.date
+    GROUP BY i.point, i.date
+UNION
+SELECT o.point, o.date, SUM(i.inc), SUM(o.out)
+    FROM Outcome_o o
+    LEFT JOIN Income_o i
+        ON i.point=o.point AND i.date=o.date
+    GROUP BY o.point, o.date
